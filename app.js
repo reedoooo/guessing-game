4ca7a3b731e70fde0fa13903037308c7728b2db5
reedoooo <readvogt@gmail.com>
2023-01-10 15:15:03 -0700

    New project called guessing game

---
+++ app.js
@@ -0,0 +1,128 @@
+'use strict';
+
+let greeting = prompt('What is your name?')
+alert('hello ' + greeting + ' welcome to my site.')
+
+let question1 = prompt('Is my name Reed?');
+if (question1 === ('y').toUpperCase()){
+    alert('Correct!');
+    console.log('correct')
+} else if (question1 === ('y').toLowerCase()){
+    alert('Wrong case')
+    console.log('false')
+} else if (question1 === 'n'.toUpperCase()){
+    alert('Wrong Answer')
+    console.log('false')
+} else if (question1 === 'n'.toLowerCase()){
+    alert('Wrong Answer, wrong case')
+    console.log('false')
+} else {
+    alert('enter a real answer')
+    console.log('false')
+}
+let question2 = prompt('Is my age 30?');
+if (question2 === ('n').toUpperCase()){
+    alert('Correct!');
+    console.log('correct')
+} else if (question2 === ('n').toLowerCase()){
+    alert('Wrong case')
+    console.log('false')
+} else if (question2 === 'y'.toUpperCase()){
+    alert('Wrong Answer')
+    console.log('false')
+} else if (question2 === 'y'.toLowerCase()){
+    alert('Wrong Answer, wrong case')
+    console.log('false')
+} else {
+    alert('enter a real answer')
+    console.log('false')
+}
+let question3 = prompt('Is my hair long?');
+if (question3 === ('y').toUpperCase()){
+    alert('Correct!');
+    console.log('correct')
+} else if (question3 === ('y').toLowerCase()){
+    alert('Wrong case')
+    console.log('false')
+} else if (question3 === 'n'.toUpperCase()){
+    alert('Wrong Answer')
+    console.log('false')
+} else if (question3 === 'n'.toLowerCase()){
+    alert('Wrong Answer, wrong case')
+    console.log('false')
+} else {
+    alert('enter a real answer')
+    console.log('false')
+}
+let question4 = prompt('Am I a girl?');
+if (question4 === ('n').toUpperCase()){
+    alert('Correct!');
+    console.log('correct')
+} else if (question4 === ('n').toLowerCase()){
+    alert('Wrong case')
+    console.log('false')
+} else if (question4 === 'y'.toUpperCase()){
+    alert('Wrong Answer')
+    console.log('false')
+} else if (question4 === 'y'.toLowerCase()){
+    alert('Wrong Answer, wrong case')
+    console.log('false')
+} else {
+    alert('enter a real answer')
+    console.log('false')
+}
+let question5 = prompt('Is my eye color brown?')
+if (question5 === ('y').toUpperCase()){
+    alert('Correct!');
+    console.log('correct')
+} else if (question5 === ('y').toLowerCase()){
+    alert('Wrong case')
+    console.log('false')
+} else if (question5 === 'n'.toUpperCase()){
+    alert('Wrong Answer')
+    console.log('false')
+} else if (question5 === 'n'.toLowerCase()){
+    alert('Wrong Answer, wrong case')
+    console.log('false')
+} else {
+    alert('enter a real answer')
+    console.log('false')
+}
+
+alert('goodbye ' + greeting + ' thanks for playing')
+/*
+if(answer1 !== answer1){
+    alert('Correct!');
+} else if (question1 === question1.toLowerCase()){
+    alert('Wrong Answer')
+} else if (question1 === question1.toLowerCase()){
+    alert('Wrong Case.')
+} else {
+    alert('wrong')
+}
+*/
+/*
+let question2 = prompt('Is my age 30?');
+let question3 = prompt('Is my hair long?');
+let question4 = prompt('Am I a girl?');
+    if(question1 === 'Y'){
+        alert('Correct!');
+    } else if (question1 === 'N'){
+        alert('Wrong Answer')
+    } else {
+        alert('Wrong Case.')
+    }
+
+    
+    if(question2 === 'Y'){
+        alert('Try again');
+    }
+    if(question3 === 'Y'){
+        alert('Try again');
+    }
+    if(question4 === 'Y'){
+        alert('Try again');
+    }
+*/
+
+
